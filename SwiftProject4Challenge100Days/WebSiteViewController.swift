69c3508fcb4a3d11ae526bb75c15627e19c94778
saman <abdallaelsaman67@gmail.com>
2019-02-28 15:30:41 +0200

    creating WebSiteViewController and attached to UI

---
+++ SwiftProject4Challenge100Days/WebSiteViewController.swift
@@ -0,0 +1,18 @@
+//
+//  WebSiteViewController.swift
+//  SwiftProject4Challenge100Days
+//
+//  Created by Abdalla Elsaman on 2/28/19.
+//  Copyright © 2019 Dumbies. All rights reserved.
+//
+
+import UIKit
+import WebKit
+
+class WebSiteViewController: UIViewController, WKNavigationDelegate {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
